8e5fd56d537cd080e2ce01259d7248072462dc76
Atya22 <aytaj2002eva@gmail.com>
2024-03-28 13:00:23 +0000

    Next task

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 unsigned int write_length;
 void _putchar(char c);
 void print_num(int num);
+void printnum_full(int num);
 void simple_print(const char* str);
 int _printf(const char *format, ...);
 
